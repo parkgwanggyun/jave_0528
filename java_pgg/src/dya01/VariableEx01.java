7c3adc777b2eec150e76ee223e944a9529f8b8cc
parkgwanggyun <sonud_1@naver.com>
2024-05-28 17:10:53 +0900

    자바1일차 변수선언예제

---
+++ java_pgg/src/dya01/VariableEx01.java
@@ -0,0 +1,22 @@
+package dya01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+		
+		/*char ch1;//문자형 변수 ch1를 선언함
+		char ch2 = '문'; //문자형 변수 ch2를 선언하고 A로 초기화
+		char ch3, ch4; //문자형 변수 ch3과ch4를 선언 같은 자료형인 경우 ,를 통해 동시에 선언할 수 있습니다.
+		char ch5 = '한' , ch6 = '\\'; //문자형 변수 ch5를 선언하고 한으로 초기화 ,문자형 변수 ch6를 선언하고 \n로 초기화
+		System.out.println(ch2);
+		System.out.println(ch6);
+		System.out.println(ch5);
+		*/
+		//정수는 리터럴은 기본이 int 실수는 리터럴은 기본이 double
+		long num1 = 1234567890123L;//정수값이 int형으로 표현되지 않으면 접미사(L,l)를 반드시 붙여야합니다.
+		float num2 = 3.14f; //3.14는 double형이기 때문에 반드시 접미사를 붙여야 합니다.
+		double num3 = 3.1424;
+		boolean isEven = true;
+	}
+
+}
