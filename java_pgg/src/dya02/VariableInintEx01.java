b9e142530f217453d71624396365327123f5aba9
parkgwanggyun <sonud_1@naver.com>
2024-05-29 12:47:36 +0900

    자바2일차 변수값 초기화

---
+++ java_pgg/src/dya02/VariableInintEx01.java
@@ -0,0 +1,15 @@
+package dya02;
+
+public class VariableInintEx01 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		//System.out.println(num); //num를 초기화 하지 않아서 에러 발생
+		
+		num = 0;
+		
+		System.out.println(num); //num를 초기화 해서 에러 발생하지 않음
+	}
+
+}
