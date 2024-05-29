593f2526a526736826aa728196e1f4a24ad5e341
parkgwanggyun <sonud_1@naver.com>
2024-05-29 12:47:15 +0900

    자바2일차 변수명 작성 관례

---
+++ java_pgg/src/dya02/VariableEx03.java
@@ -0,0 +1,14 @@
+package dya02;
+
+public class VariableEx03 {
+	//변수명 작성 관례
+	public static void main(String[] args) {
+		int Num, num; //Num대신 num를 추천
+		int totalcount, totalCount; //totalcount 대신 totalCount를 추천(카멜 표기법)
+		
+		final int maxNum = 10, Max_NUM = 10; //상수는 대문자로 표현하는걸 추천
+		//MAX_NUM = 20; //위에서 값을 한 번 저장했기 때문에 상수는 값을 더 수정할 수 없다.
+		int age , a; //나이를 의미하는 변수로 age를 추천		
+	}
+
+}
