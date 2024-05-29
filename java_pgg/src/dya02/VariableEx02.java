dc8f23c9e2d5cb6154358a621ac6376ba01169c9
parkgwanggyun <sonud_1@naver.com>
2024-05-29 12:47:02 +0900

    자바2일차 변수명 작성규칙

---
+++ java_pgg/src/dya02/VariableEx02.java
@@ -0,0 +1,21 @@
+package dya02;
+
+public class VariableEx02 {
+	//변수명 작성 규칙
+	public static void main(String[] args) {
+		int num;
+		int NUM, Num, nUm, nuM; //대소문자를 구별하기 때문에 가능
+		
+		//int int,char,class,public; //예약어 사용 불가
+		
+		int _num,n_um,$num;
+		//int n um, #num; //특수문자는 _과$만 가능
+		
+		int num1;
+		//int 1num; //숫자는 앞에 올 수 없다.
+		
+		//int num; // 중복 선언 불가능 , 현재 우치에서 사용중인 변수가 1개인 경우 사용 가능
+		
+	}
+
+}
