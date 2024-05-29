3f3d64d0e0d5e2e8d21ff2448d0f1158440befc5
parkgwanggyun <sonud_1@naver.com>
2024-05-29 17:35:40 +0900

    자바2일차 Scanner 이용하여 계산기 구현해보기

---
+++ java_pgg/src/dya02/ScannerEx.java
@@ -0,0 +1,23 @@
+package dya02;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("첫번째 정수를 입력하세요 : ");
+		int first = scanner.nextInt();
+		System.out.print("두번째 정수를 입력하세요 : ");
+		int second = scanner.nextInt();
+		
+		System.out.println("더하기 결과 : "+ (first+second));
+		System.out.println("빼기 결과 : "+ (first-second));
+		System.out.println("나누기 결과 : "+ (first/second));
+		System.out.println("나머지 결과 : "+ (first%second));
+		System.out.println("곱하기 결과 : "+ (first*second));
+	}
+
+}
---
+++ java_pgg/src/dya02/ScannerEx04.java
@@ -0,0 +1,27 @@
+package dya02;
+
+import java.util.Scanner;
+
+public class ScannerEx04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("연산을 할 첫번째 숫자를 입력하세요 :");
+		double num1 = Double.parseDouble(sc.nextLine());
+		
+		System.out.println("연산을 할 두번째 숫자를 입력하세요 :");
+		double num2 = Double.parseDouble(sc.nextLine());
+		System.out.println("어떤 연산을 수행하시겠습니까?" + ("+(더하기), - (빼기), *(곱하기), / (나누기), %(나머지)"));
+		String user = sc.nextLine();
+		
+		/*String result =
+				user.equals("+")? ""+(num1+num2) :
+				(user.equals("-")? ""+(num1-num2)	:
+					(user.equals("/")? ""+(Math.round((num1/num2)*10)/10.0):
+						(user.equals("*")? ""+(Math.round((num1*num2)*10)/10.0) :
+							(user.equals("%")? ""+(num1%num2):"확인불가") )));
+							System.out.println(result);*/
+	}
+
+}
