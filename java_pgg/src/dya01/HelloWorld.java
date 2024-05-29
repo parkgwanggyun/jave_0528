ce94e888e4286f6ca6a9a56360a8b06e10a99507
parkgwanggyun <sonud_1@naver.com>
2024-05-29 12:47:56 +0900

    자바2일차 헬로우월드 안녕으로 바꿔보기

--- java_pgg/src/dya01/HelloWorld.java
+++ java_pgg/src/dya01/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) { //← 프로그램을 실행하기위해서는 main 명령어가 필요하다
 		// ←System.out.println() : 콘솔에 원하는 문구를 출력하는 기능입니다.
 		
-		System.out.println("Hello World!"); // ←한줄 주석 표현
+		System.out.println("안녕"); // ←한줄 주석 표현
 		
 	/*
 	 	
