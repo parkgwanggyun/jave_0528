94bc9737a939b9cb013ed3a87e91ce5277120d79
parkgwanggyun <sonud_1@naver.com>
2024-05-29 12:45:13 +0900

    String 예제풀기

---
+++ java_pgg/src/dya02/Exam02.java
@@ -0,0 +1,14 @@
+package dya02;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		int num1 = 1 , num2 = 2;
+		char ch = '+';
+		
+		String str1 = ""+num1 + ch + num2;
+		System.out.println("" + num1 + ch + num2);
+		System.out.println(str1);
+	}
+
+}
