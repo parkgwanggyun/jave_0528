ac90a7404382df773008dbf8f8795d77b8511ff3
parkgwanggyun <sonud_1@naver.com>
2024-05-29 12:45:38 +0900

    자바2일차 각 예약문으로 0표현하기

---
+++ java_pgg/src/dya02/Exam1.java
@@ -0,0 +1,24 @@
+package dya02;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		
+		char ch1 = '0';
+		
+		int ch2 = 0;
+		
+		long ch3 = 0L;
+		
+		double ch4 = 0.0;
+		
+		float ch5 = 0.0f;
+		
+		System.out.println(ch2);
+		System.out.println(ch1);
+		System.out.println(ch3);
+		System.out.println(ch4);
+		System.out.println(ch5);
+	}
+
+}
