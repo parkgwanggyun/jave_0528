063bbc96a6dc3c6d31e562c27e3ff361af737cf3
parkgwanggyun <sonud_1@naver.com>
2024-05-30 12:49:14 +0900

    자바3일차 else if예제

---
+++ java_pgg/src/day03/IfEx03.java
@@ -0,0 +1,26 @@
+package day03;
+
+public class IfEx03 {
+
+	public static void main(String[] args) {
+		//else if예제
+		int num = 0;
+		//num가 0이면 0이라고 출력하고
+		if(num == 0) {
+			System.out.println("0입니다");
+		}
+		else if(num >= 1){
+			System.out.println("양수입니다");
+			
+		}
+		else {
+			System.out.println("음수입니다.");
+		}
+		
+		//num가 0이 아니고 양수이면 양수라고 출력하고
+		
+		//num가 0과 양수가 아니면 음수라고 출력하는 코드를 작성하세요
+
+	}
+
+}
