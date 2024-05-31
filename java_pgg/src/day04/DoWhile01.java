a344a2e6fdd2d25fc28124e2302c81a2b9c9ea1d
parkgwanggyun <sonud_1@naver.com>
2024-05-31 17:11:23 +0900

    자바4일차 DoWhile문 예제

---
+++ java_pgg/src/day04/DoWhile01.java
@@ -0,0 +1,18 @@
+package day04;
+
+import java.util.Scanner;
+
+public class DoWhile01 {
+
+	public static void main(String[] args) {
+		//y를 입력하면 종료하는 코드를 작성하세요
+		Scanner scan = new Scanner(System.in);
+		char ch;
+		do {
+			System.out.println("문자");
+			ch = scan.next().charAt(0);
+		}while(ch != 'y');
+
+	}
+
+}
