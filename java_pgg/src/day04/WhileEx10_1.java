7041f1ecb1b242895c2b01d67a0000fe07f7a384
parkgwanggyun <sonud_1@naver.com>
2024-05-31 17:14:50 +0900

    자바4일차 while문 예제

---
+++ java_pgg/src/day04/WhileEx10_1.java
@@ -0,0 +1,16 @@
+package day04;
+
+import java.util.Scanner;
+
+public class WhileEx10_1 {
+
+	public static void main(String[] args) {
+		char ch = 'n';
+		Scanner scan = new Scanner(System.in);
+		while(ch != 'y') {
+			System.out.println();
+			char A = scan.next().charAt(0);
+		}
+	}
+
+}
