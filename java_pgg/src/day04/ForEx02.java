732dfbe6f20d4c6cd3a0a596a0f17cb4bc7ba4d3
parkgwanggyun <sonud_1@naver.com>
2024-05-31 17:12:36 +0900

    자바4일차 for문 구구단2단예제

---
+++ java_pgg/src/day04/ForEx02.java
@@ -0,0 +1,19 @@
+package day04;
+
+public class ForEx02 {
+
+	public static void main(String[] args) {
+		/* 구구단 2단을 출력하는 코드를 for문을 이용하여 작성하세요.
+		 * 
+		 */
+		
+		int num1 = 2;
+		int i;
+		for(i=1;i<=9; ++i) {
+			System.out.println(num1 + "x" + i + "=" + (num1*i));
+			
+		}
+
+	}
+
+}
