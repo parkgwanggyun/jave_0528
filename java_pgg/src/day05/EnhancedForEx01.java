5ca0039018f4174fbda7c5a3adf3540e9d3d1b1b
parkgwanggyun <sonud_1@naver.com>
2024-06-03 17:26:39 +0900

    자바5일차 배열 저장

---
+++ java_pgg/src/day05/EnhancedForEx01.java
@@ -0,0 +1,20 @@
+package day05;
+
+public class EnhancedForEx01 {
+
+	public static void main(String[] args) {
+		//배열에 1,2,3,4,5를 저장한 후 향상된 for문을 이용하여 조회하는 예제
+		int [] arr = new int[] {1,2,3,4,5} ;
+		
+		for(int num : arr ) {
+			System.out.print(num + ",");
+			
+		}System.out.println();
+		for(int i = 0; i<arr.length; i++) {
+			int num = arr[i];
+			System.out.print(num + ",");
+		}
+
+	}
+
+}
