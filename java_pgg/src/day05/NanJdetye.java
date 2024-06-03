ae2573669222f4b4f33eb595d32761c546ca5683
parkgwanggyun <sonud_1@naver.com>
2024-06-03 17:26:48 +0900

    자바5일차 반복문 개인연습

---
+++ java_pgg/src/day05/NanJdetye.java
@@ -0,0 +1,14 @@
+package day05;
+
+public class NanJdetye {
+
+	public static void main(String[] args) {
+		// 1부터 100까지의 모든 짝수를 출력하세요
+		for(int i=1; i <= 100; i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
