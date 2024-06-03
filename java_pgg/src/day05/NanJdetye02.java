0a2d17cb6f38e422ab1a47dd8cf60dcba332ed98
parkgwanggyun <sonud_1@naver.com>
2024-06-03 17:26:55 +0900

    자바5일차 반복문 개인연습

---
+++ java_pgg/src/day05/NanJdetye02.java
@@ -0,0 +1,15 @@
+package day05;
+
+public class NanJdetye02 {
+
+	public static void main(String[] args) {
+		//1부터 10까지의 모든 정수를 역순으로 출력하세요
+		int i = 10;
+		while(i >= 1) {	
+			System.out.print(i);
+			--i;
+			
+		}
+	}
+
+}
