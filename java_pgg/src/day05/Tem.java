6b2304e743ac14797b3d51144092d0a93ef7d9c6
parkgwanggyun <sonud_1@naver.com>
2024-06-03 12:45:35 +0900

    자바5일차  향상된 for문예제

---
+++ java_pgg/src/day05/Tem.java
@@ -0,0 +1,12 @@
+package day05;
+
+public class Tem {
+
+	public static void main(String[] args) {
+		int [] arr = new int[] {1,2,3,4,5};
+		for(int tmp : arr) {
+			System.out.print(tmp);
+			}
+	}
+
+}
