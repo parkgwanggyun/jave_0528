ad20fb82e573a78ce7d27be882de0813d93cfd1f
parkgwanggyun <sonud_1@naver.com>
2024-06-04 17:43:19 +0900

    자바6일차 숙제풀이

--- java_pgg/src/day06/ArrayEx09.java
+++ java_pgg/src/day06/ArrayEx09.java
@@ -6,26 +6,27 @@ public class ArrayEx09 {
 
 	public static void main(String[] args) {
 		String [] list = {"dog" , "cat" , "java", "cup", "computer"};
-		boolean str = true;
+		boolean str = false;
 		//문자열을 입력받아 입력받은 문자열이 있는지 없는지 알려주는 코드를 작성하세요
 		//입력받아야해서 스캐너를 넣고
 		Scanner sc = new Scanner(System.in);
 		//배열에 문자를 넣고
-		for(int i = 0; i < list.length; i++) {
-			
-
-		}for(String tmp : list) {
-			System.out.print("문자를 입력하세요 : ");
-			tmp = sc.next();
-			if(tmp.equals(list.length)) {
-				str = false;
+		System.out.print("문자를 입력하세요 : ");
+		String why = sc.next();
+		
+		for(String tmp : list) {
+			if(tmp.equals(why)) {
+				str = true;
 				break;
-			}
-			if(str = true) {
-				System.out.println("아닙니다.");
-			}if (str = false){
-				System.out.println("있습니다.");
-			}
+			}			
+			
+		}
+		if(str) {
+			System.out.println("입력하신"+why+"맞습니다.");
+		}
+		
+		else{
+			System.out.println("입력하신" + why+ "없습니다.");
 		}
 	}
 }
