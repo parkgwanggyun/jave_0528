3495b1f4788a19d5702bc7c43ed813d7cda705a4
parkgwanggyun <sonud_1@naver.com>
2024-06-05 17:50:44 +0900

    홈워크

--- java_pgg/src/day07/homework/BaseballGame.java
+++ java_pgg/src/day07/homework/BaseballGame.java
@@ -1,5 +1,7 @@
 package day07.homework;
 
+import java.util.Scanner;
+
 public class BaseballGame {
 
 	public static void main(String[] args) {
@@ -23,9 +25,37 @@ public class BaseballGame {
 		 * 3S
 		 * 정답입니다.
 		 * */
-		//랜덤한 수를 생성
-		//랜덤한수를 배열에 입력
-		//입력
+		//1~9사이의 랜덤한 수를 생성
+		//생성된 랜덤한수를 배열에 입력
+		//스캐너를 이용하여 콘솔에 입력
+		//랜덤한 수와 콘솔에 입력된수를 비교
+		//배열 저장된 위치 와 숫자가 맞으면 스트라이트
+		//배열 저장된 숫자가 맞으면 볼
+		//하나도 맞는게 없다면 아웃
+		//배열 저장된 위치 와 숫자가 3개 다 맞추면 3스트라이크
+		//3스트라이크 시 승리 출력
+		
+		//3개의 랜덤한 숫자를 저장할수있는 배열을 생성
+		int randomnumber[] = new int[3];
+		//1~9사이의 랜덤한 숫자를 만들기위한 값 부여
+		int min = 1 , max = 9;
+		int count = 0;
+		int random;
+		Scanner sc = new Scanner(System.in);
+		//랜덤한수를 배열하고 중복을 걸러주기 위한 반복문 (ArrayEx02참고)
+		while(count < 3) {
+			random = (int)(Math.random() * (max - min + 1) + min);
+			for(int i = 0; i < count; i++) {
+				if(randomnumber[i] == random) {
+					break;
+				}if(i == count) {
+					randomnumber[count] = random;
+					count++;
+				}
+			}
+		}
+		//랜덤한 수 생성
+		
 	}
 
 }
