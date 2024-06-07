d4846bf44d965373163c38733976fab8d4494b10
parkgwanggyun <sonud_1@naver.com>
2024-06-07 16:47:53 +0900

    자바8일차 베이스볼게임예제

--- java_pgg/src/day07/homework/BaseballGame.java
+++ java_pgg/src/day07/homework/BaseballGame.java
@@ -1,5 +1,6 @@
 package day07.homework;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BaseballGame {
@@ -34,28 +35,111 @@ public class BaseballGame {
 		//하나도 맞는게 없다면 아웃
 		//배열 저장된 위치 와 숫자가 3개 다 맞추면 3스트라이크
 		//3스트라이크 시 승리 출력
-		
+
 		//3개의 랜덤한 숫자를 저장할수있는 배열을 생성
-		int randomnumber[] = new int[3];
-		//1~9사이의 랜덤한 숫자를 만들기위한 값 부여
-		int min = 1 , max = 9;
+		int list [] = new int[3];
 		int count = 0;
+		int min =1 , max = 9;
 		int random;
-		Scanner sc = new Scanner(System.in);
-		//랜덤한수를 배열하고 중복을 걸러주기 위한 반복문 (ArrayEx02참고)
-		while(count < 3) {
-			random = (int)(Math.random() * (max - min + 1) + min);
-			for(int i = 0; i < count; i++) {
-				if(randomnumber[i] == random) {
-					break;
-				}if(i == count) {
-					randomnumber[count] = random;
+		int i;
+		int s =0 , b=0;
+		do {
+			s = 0;
+			b = 0;
+			while(count < 3) {
+				random = (int)(Math.random() * (max - min + 1) + min);
+				for(i =0; i < count; i++) {
+					if(list[i] == random) {
+						break;//break를 만나면 i는 count보다 작을수밖에 없음
+					}
+				}
+				if(i == count) {
+					list[count] = random;
 					count++;
 				}
 			}
-		}
-		//랜덤한 수 생성
-		
-	}
+			System.out.println(Arrays.toString(list));
+			Scanner sc = new Scanner(System.in);
+			int [] user = new int[3];
 
+			System.out.print("입력 : ");
+			//3개입력
+			for(int k = 0; k <user.length; k++) {
+				user[k] = sc.nextInt();
+			}		
+			for(i = 0; i < user.length; i++) {
+				for(int j =0; j < user.length; j++) {
+					if(list[i] == user[j]) {
+						if(i==j) {
+							s++;
+						}else {
+							b++;
+						}
+					}
+				}
+				/*for(int j =0; j < user.length; j++) {
+			if(list[0] == user[j]) {
+				if(0==j) {
+					s++;
+				}else {
+					b++;
+				}
+			}
+		}
+		for(int j =0; j < user.length; j++) {
+			if(list[1] == user[j]) {
+				if(1==j) {
+					s++;
+				}else {
+					b++;
+				}
+			}
+		}
+
+		for(int j =0; j < user.length; j++) {
+			if(list[2] == user[j]) {
+				if(2==j) {
+					s++;
+				}else {
+					b++;
+				}
+			}*/
+
+				if(s != 0) {
+					System.out.print(s + "S");
+				}if (b != 0) {
+					System.out.print(b + "B");
+				}if(s == 0 && b ==0) {
+					System.out.print("O");
+				}System.out.println();
+
+			}
+		}while(s != 3);
+		System.out.println("정답입니다.");
+
+	}
 }
+
+		//반복문을 이용하여 정수 3개를 입력 받은 후 판별(다 맞출 때까지)		
+		//입력 안내문구 출력
+		//3개를 입력
+		//결과 판별 후 출력
+		//첫번째 랜덤수와 첫번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//첫번째 랜덤수와 두번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//첫번째 랜덤수와 세번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//두번째 랜덤수와 첫번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//두번째 랜덤수와 두번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//두번째 랜덤수와 세번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//세번째 랜덤수와 첫번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//세번째 랜덤수와 두번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+		//세번째 랜덤수와 세번째 입력값이 같으면 스트라이크 아니면 볼로 판별
+					
+
