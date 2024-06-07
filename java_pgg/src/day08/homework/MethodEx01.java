e08d6f29bcd34035455f6463b3bb0e0969381005
parkgwanggyun <sonud_1@naver.com>
2024-06-07 17:44:56 +0900

    자바8일차 홈워크

--- java_pgg/src/day08/homework/MethodEx01.java
+++ java_pgg/src/day08/homework/MethodEx01.java
@@ -1,10 +1,26 @@
 package day08.homework;
 
+import java.util.Scanner;
+
 public class MethodEx01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("배열의 크기를 입력하세요: ");
+	        int n = sc.nextInt();
+	        int[] arr = Arrangement(n);
+	        System.out.println("생성된 배열의 크기: " + arr.length);
+	    }
 
+	    /**
+	     * 기능 : 정수 n을 입력받아 입력받은 n만큼 배열크기를 생성
+	     * 매개변수 : int n
+	     * 리턴타입 :	int
+	     * 메서드명 :	Arrangement
+	     *
+	     */
+	    public static int[] Arrangement(int n) {
+	        int[] arr = new int[n];
+	        return arr;
+	    }
 	}
-
-}
