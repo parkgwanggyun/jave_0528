5059bed6117f10c750ee5803987853588cb37d33
parkgwanggyun <sonud_1@naver.com>
2024-06-07 17:44:59 +0900

    자바8일차 홈워크

--- java_pgg/src/day08/homework/MethodEx02.java
+++ java_pgg/src/day08/homework/MethodEx02.java
@@ -1,10 +1,29 @@
 package day08.homework;
 
 public class MethodEx02 {
-
+	/*1~9사이의 랜덤한 수를 배열에 저장하여 콘솔에 출력하는 코드를 작성하세요
+	 * 메서드 이용
+	 * */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int []arr = new int[3];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = random();
+		}
+		for(int num : arr) {
+			System.out.println(num);
+		}
+		
+	}
+	/**기능 : 1~9사이의 랜덤한 수를 만드는 기능
+	 * 매개변수 : 
+	 * 리턴타입 : int
+	 * 메서드명 : random
+	 * */
+	public static int random() {
+		int min =1 , max = 9;		
+		int random = (int)(Math.random() * (max - min + 1) + min);
+			
+		return random;
+		}
 	}
 
-}
