59806b399e74efe00e138c6f91881720ccd3394c
parkgwanggyun <sonud_1@naver.com>
2024-06-14 17:02:47 +0900

    자바 13일차 예외처리 예제

---
+++ java_pgg/src/day13/ExceptionEx05.java
@@ -0,0 +1,34 @@
+package day13;
+
+public class ExceptionEx05 {
+
+	public static void main(String[] args) {
+		try {
+			test();
+		} catch (MyException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+	public static void test() throws MyException {
+		try {
+			System.out.println(1/0);
+		}catch(Exception e) {
+			throw new MyException("test", "0으로 나눌수 없습니다.");
+		}
+	}
+}
+
+class MyException extends Exception{
+	
+	String who;
+	public MyException(String messge, String who) {
+		super(messge);
+		this.who = who;
+	}
+	@Override
+	public String getMessage() {
+		return who + " : " + super.getMessage();
+	}
+}
