7a175ce888cc474c7c5fce551992831eef7505e8
parkgwanggyun <sonud_1@naver.com>
2024-06-17 17:01:15 +0900

    자바14일차 홈워크해문2

--- java_pgg/src/day14/Contact.java
+++ java_pgg/src/day14/Contact.java
@@ -1,5 +1,13 @@
 package day14;
 
 public class Contact {
+	
+	private String name;
+	private String number;
+	
+	public Contact(String name, String number) throws Exception {
+		this.name = name;
+		setNumber(number);
+	}
 
 }
