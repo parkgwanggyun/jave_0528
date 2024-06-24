6847eab6d898ef8bc6e4d2fdf298c9b96d256729
parkgwanggyun <sonud_1@naver.com>
2024-06-24 17:13:42 +0900

    자바19일차 Synchronized예제

--- java_pgg/src/day18/SynchronizedEx02.java
+++ java_pgg/src/day18/SynchronizedEx02.java
@@ -6,39 +6,41 @@ import lombok.Data;
 public class SynchronizedEx02 {
 
 	public static void main(String[] args) {
-		// 은행 잔고를 통한 동기화 블록 예제
-		BankBook bankBook = new BankBook("홍길동", 0);
+		//은행 잔고를 통한 동기화 블록 예제
+		BankBook2 bankBook = new BankBook2("홍길동", 0);
 		Customer c1 = new Customer("홍길동", bankBook);
 		Customer c2 = new Customer("홍아무개", bankBook);
-
-		Thread t1 = new Thread(() -> c1.deposit(10000));
-		Thread t2 = new Thread(() -> c2.deposit(10000));
+		
+		Thread t1 = new Thread(()->c1.deposit(10000));
+		Thread t2 = new Thread(()->c2.deposit(10000));
+		t1.start();
+		t2.start();
 	}
 }
 @AllArgsConstructor
-	class Customer {
-		private String name;
-		private BankBook2 bankBook;
-
-		public void deposit(int money) {
-			synchronized (bankBook) {
-				int balance = bankBook.getBalance();
-				bankBook.setBalance(balance + money);
-
-				try {
-					// 현재 쓰레드를 2초간 멈춤
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println(name + " : " + money + "원");
+class Customer{
+	private String name;
+	private BankBook2 bankBook;
+	
+	public void deposit(int money) {
+		System.out.println(name + "님이 입금중입니다......" );
+		synchronized (bankBook) {
+			int balance = bankBook.getBalance();
+			bankBook.setBalance(balance + money);
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+			System.out.println(name + " : " + money + "원 : " + bankBook.getBalance() + "원");
+			
 		}
 	}
-
+}
 @Data
 @AllArgsConstructor
-class BankBook2 {
+class BankBook2{
 	private String name;
 	private int balance;
 }
