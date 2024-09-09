06cc88509e551ae9f9507c9347a09844743504f7
parkgwanggyun <sonud_1@naver.com>
2024-09-09 10:33:29 +0900

    리액트 1일차 리액트 프로젝트 생성 및 button생성

---
+++ react/react1/src/App.js
@@ -0,0 +1,16 @@
+//import './css/App.css'; //css폴더에 App.css가 있고,연결할 때 사용
+import Button from "./Button";
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <button>버튼</button>
+        <Button />
+      </div>
+    </div>   
+  );
+}
+
+export default App;
---
+++ react/react1/src/Button.js
@@ -0,0 +1,7 @@
+function Button(){
+    return (
+        <button>버튼입니다.</button>
+    );
+}
+
+export default Button;
